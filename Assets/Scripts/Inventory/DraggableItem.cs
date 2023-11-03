3eb0aee4fbbcde32b0dc72d2d84b7d2e3da78c1a
beebek <limbubeebek@gmail.com>
2023-11-03 13:16:18 +0000

    Inventory added with cursor toggle

--- Assets/Scripts/Inventory/DraggableItem.cs
+++ Assets/Scripts/Inventory/DraggableItem.cs
@@ -6,11 +6,10 @@ using UnityEngine.EventSystems;
 
 public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    public Image image;
+    public Image image; 
     [HideInInspector] public Transform parentAfterDrag;
 
     public void OnBeginDrag(PointerEventData eventData){
-        Debug.Log("Begin drag");
         parentAfterDrag = transform.parent;
         transform.SetParent(transform.root);
         transform.SetAsLastSibling();
@@ -18,15 +17,12 @@ public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
     }
 
     public void OnDrag(PointerEventData eventData){
-        Debug.Log("Begin drag");
         transform.position = Input.mousePosition;
     }
 
     public void OnEndDrag(PointerEventData eventData){
-        Debug.Log("Begin drag");
         transform.SetParent(parentAfterDrag);
         image.raycastTarget = true;
     }
-
-     
+   
 }
--- Assets/Scripts/Inventory/InventorySlot.cs
+++ Assets/Scripts/Inventory/InventorySlot.cs
@@ -6,11 +6,11 @@ using UnityEngine.EventSystems;
 public class InventorySlot : MonoBehaviour, IDropHandler
 {   
     public void OnDrop(PointerEventData eventData){
-        if (transform.childCount == 0){
+        
             GameObject dropped = eventData.pointerDrag;
             DraggableItem draggableItem = dropped.GetComponent<DraggableItem>();
             draggableItem.parentAfterDrag = transform;
-        }
+        
         
     }
 }
