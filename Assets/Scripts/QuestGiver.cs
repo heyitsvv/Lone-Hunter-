42a6881d6d32e269ff22242e722451d027ebddfc
Vivinia Asyiqin Binti Noralam <89991833+heyitsvv@users.noreply.github.com>
2023-11-05 12:51:51 +0000

    Toggl cursor [Quest]

--- Assets/Scripts/QuestGiver.cs
+++ Assets/Scripts/QuestGiver.cs
@@ -8,16 +8,18 @@ public class QuestGiver : MonoBehaviour
     public Quest quest;
     public PlayerMovement player;
 
-    private string[] animals = { "Wolf", "Bear", "Cattle" };
+    private string[] animals = { "Wolf"};
     private string[] Title = { "Next Quest", "Hunting Challenge", "On The Hunt" };
     private int index;
     private int numberKill;
+    private bool isQuestOpen;
 
     public TextMeshProUGUI titleText;
     public TextMeshProUGUI descText;
 
     public void Start()
     {
+        isQuestOpen = false;
         titleText.text = quest.title;
         descText.text = quest.description;
         index = 0;
@@ -26,9 +28,29 @@ public class QuestGiver : MonoBehaviour
     public void CompleteQuest()
     {
         index = Random.Range(0, animals.Length);
-        numberKill = Random.Range(1, 2);
+        numberKill = Random.Range(1, 4);
         titleText.text = Title[index];
-        descText.text = "Venture out into the wild and kill " + numberKill + " " + animals[index];
+        descText.text = "Venture out into the wild and kill " + numberKill + " " + animals[0];
 
+    }
+
+
+    public void questWindow (bool state)
+    {
+        isQuestOpen = state;
+    }
+
+    public void ToggleCursorState()
+    {
+        if (isQuestOpen)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
     }
 }
