f0da5a04961ffc45d50419eaba3084c808651f47
beebek <b.limbu@se21.qmul.ac.uk>
2024-01-07 14:45:08 +0000

    responsive inventory

--- Assets/Scripts/Inventory/InventorySlot.cs
+++ Assets/Scripts/Inventory/InventorySlot.cs
@@ -1,20 +1,48 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.Collections;
 
 public class InventorySlot : MonoBehaviour, IDropHandler
 {
+    public Item item;
+    public Image icon;
+    public Inventory inventory;
+    public void UpdateSlot()
+    {
+        if(item != null)
+        {
+            icon.sprite = item.icon; // No need for GetComponent<Image>() as icon is already an Image
+            icon.gameObject.SetActive(true); // Activate the GameObject that the icon is attached to
+        }
+        else
+        {
+            icon.gameObject.SetActive(false); // Deactivate the GameObject
+        }
+    }
+
     public void OnDrop(PointerEventData eventData)
     {
-        GameObject dropped = eventData.pointerDrag;
-        DraggableItem draggableItem = dropped.GetComponent<DraggableItem>();
+        GameObject droppedObject = eventData.pointerDrag;
+        DraggableItem draggableItem = droppedObject.GetComponent<DraggableItem>();
 
         if (draggableItem != null)
         {
-            // Set the parent of the dragged item to the inventory slot
-            draggableItem.parentAfterDrag = transform;
-            dropped.transform.SetParent(transform);
+            InventorySlot sourceSlot = draggableItem.GetOriginalSlot();
+            if (sourceSlot != this)
+            {
+                // Swap items between slots
+                Item tempItem = item;
+                item = sourceSlot.item;
+                sourceSlot.item = tempItem;
 
-            // Additional logic for handling the drop, if needed
+                // Update both slots
+                UpdateSlot();
+                sourceSlot.UpdateSlot();
+
+                // Update the itemList in the Inventory
+                inventory.UpdateItemList(this, sourceSlot);
+            }
         }
     }
 }
